a478206011a0ad9936268afa1b3f30e4afad2ea9
poma <poma6185@gmail.com>
2024-01-03 02:18:52 -0400

    v7.8

--- src/config/app.js
+++ src/config/app.js
@@ -1,5 +1,6 @@
 const app = {
-    BASE_URL: 'http://192.168.1.100:8005',
+    //BASE_URL: 'http://192.168.1.100:8005',
+    BASE_URL: 'https://desarrolladoras.core-api.constructoramagc.com',
     PREFIX_URL: '/api',
 }
 
